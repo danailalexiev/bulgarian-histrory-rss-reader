5457e41cf6b2d40fd5e66f75c78abb61c02e9035
Danail Alexiev <danailalexiev@192-168-0-103.nadejda.net>
2019-01-10 03:18:07 +0200

    Improve the RSS item parsing.

--- app/src/main/java/bg/dalexiev/bgHistroryRss/data/rss/RssParser.kt
+++ app/src/main/java/bg/dalexiev/bgHistroryRss/data/rss/RssParser.kt
@@ -1,9 +1,16 @@
 package bg.dalexiev.bgHistroryRss.data.rss
 
+import android.net.Uri
+import android.text.Editable
+import android.text.Html
+import org.xml.sax.XMLReader
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.InputStream
 import java.util.*
+import java.util.regex.Pattern
+
+private const val IMG_TAG_REGEX = "<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>"
 
 class RssParser {
 
@@ -49,20 +56,28 @@ class RssParser {
     private fun parseItem(xmlParser: XmlPullParser): RssFeedItem {
         var guid = ""
         var title = ""
+        var imageUrl = ""
         var description = ""
         var link = ""
         var publishDate = Calendar.getInstance()
         var creator = ""
+        val categories = mutableListOf<String>()
         itemLoop@ while (true) {
             when (xmlParser.next()) {
                 XmlPullParser.START_TAG ->
                     when (xmlParser.name) {
-                        "guid" -> guid = xmlParser.nextText()
+                        "guid" -> guid = parseGuid(xmlParser.nextText())
                         "title" -> title = xmlParser.nextText()
-                        "description" -> description = xmlParser.nextText()
+                        "description" -> {
+                            parseDescription(xmlParser.nextText()).apply {
+                                imageUrl = first
+                                description = second
+                            }
+                        }
                         "link" -> link = xmlParser.nextText()
                         "pubDate" -> publishDate = Calendar.getInstance()
-                        "dc:creator" -> creator = xmlParser.nextText()
+                        "creator" -> creator = xmlParser.nextText()
+                        "category" -> categories.add(xmlParser.nextText())
                     }
                 XmlPullParser.END_TAG -> {
                     if ("item".equals(xmlParser.name)) {
@@ -71,6 +86,19 @@ class RssParser {
                 }
             }
         }
-        return RssFeedItem(guid = guid, title = title, description = description, publishDate = publishDate, link = link, creator = creator)
+        return RssFeedItem(guid = guid, title = title, imageUrl = imageUrl, description = description, publishDate = publishDate, link = link, creator = creator, categories = categories)
+    }
+
+    private fun parseGuid(guidLink: String) = Uri.parse(guidLink).getQueryParameter("p")!!
+
+    private fun parseDescription(rawDescription: String) : Pair<String, String> {
+        val pattern = Pattern.compile(IMG_TAG_REGEX)
+        val matcher = pattern.matcher(rawDescription)
+        return when {
+            matcher.find() -> Pair(matcher.group(1), rawDescription.substring(matcher.end()))
+            else -> Pair("", "")
+        }
     }
 }
+
+
